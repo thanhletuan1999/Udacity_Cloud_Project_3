e8ddc80bf67ac7c4801ea7aeba545fe1b1397cc3
baovh150401 <thanhletuan1999@gmail.com>
2023-12-10 23:29:00 +0700

    update external ip of reverseporxy

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'aa9e85e51910946e8b26706d121f23ff-1916968173.us-east-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://aa9e85e51910946e8b26706d121f23ff-1916968173.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
