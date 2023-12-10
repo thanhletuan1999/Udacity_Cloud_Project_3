6d8d48b97173e86f691e252da502d8bdef495626
baovh150401 <thanhletuan1999@gmail.com>
2023-12-10 16:17:21 +0700

    first commit

---
+++ udagram-api-feed/src/config/config.ts
@@ -0,0 +1,14 @@
+export const config = {
+  'username': "postgres",
+  'password': "Thanhly24112017",
+  'database': "postgres",
+  'host': "udacity-project3.csbd3syirs20.us-east-1.rds.amazonaws.com",
+  'dialect': 'postgres',
+  'aws_region': "us-east-1",
+  'aws_profile': "default",
+  'aws_media_bucket': "udacity-project3-1412",
+  'url': "http://localhost:8100",
+  'jwt': {
+    'secret': "testing",
+  },
+};
